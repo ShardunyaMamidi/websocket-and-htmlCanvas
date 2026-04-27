93fd16691fee3afa020376738b2a467052666402
shardyboi <shardunyamamidi@gmail.com>
2026-04-27 21:17:56 +0530

    Added the chat box to let users talk

---
+++ src/main/java/com/shar/websocket_stomp/model/ChatMessage.java
@@ -0,0 +1,23 @@
+package com.shar.websocket_stomp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    private String sender;  // Has the ID
+    private String content;
+    private MessageType messageType;
+
+
+    private enum MessageType { CHAT, JOIN, LEAVE }
+
+}
