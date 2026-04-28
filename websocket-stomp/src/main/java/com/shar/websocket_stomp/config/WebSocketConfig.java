31340d7ae1a4e91643357f27b18405d42b081bd0
shardyboi <shardunyamamidi@gmail.com>
2026-04-28 08:48:28 +0530

    Segregating the frontend and backend folders

--- websocket-stomp/src/main/java/com/shar/websocket_stomp/config/WebSocketConfig.java
+++ websocket-stomp/src/main/java/com/shar/websocket_stomp/config/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-game").withSockJS();
+        registry.addEndpoint("/ws-game")
+                .setAllowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .withSockJS();
     }
 }
--- websocket-stomp/src/main/java/com/shar/websocket_stomp/controller/LocationController.java
+++ websocket-stomp/src/main/java/com/shar/websocket_stomp/controller/LocationController.java
@@ -2,6 +2,7 @@ package com.shar.websocket_stomp.controller;
 
 import com.shar.websocket_stomp.model.Coordinate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,9 +12,9 @@ import org.springframework.stereotype.Controller;
 @Slf4j
 public class LocationController {
 
-    @MessageMapping("/get-location") // When this endpoint is triggered
-    @SendTo("/topic/location") // Clients subscribed to this will receive the location
-    public Coordinate getLocation(@Payload Coordinate coordinate) {
+    @MessageMapping("/{roomId}/get-location") // When this endpoint is triggered
+    @SendTo("/topic/{roomId}/location") // Clients subscribed to this will receive the location
+    public Coordinate getLocation(@DestinationVariable String roomId, @Payload Coordinate coordinate) {
         log.info("Clicked location = X : {}, Y : {}", coordinate.getX(), coordinate.getY());
         return coordinate;
     }
